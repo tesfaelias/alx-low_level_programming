e27d2f715161c8a19a32cfb744141fc5ff8db633
tesfaelias <tesfaon@yahoo.com>
2023-06-15 19:45:46 -0700

    my c project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include stdio.h
+/*
+ * a program that print a line
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#!/bin/bash
+#include stdio.h
+/*
+ * a program that prints size of a types
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float;
+printf("Size of a Char: %lu byte(s)\n, (unsigned long)sizeof(a));
+return(0);
+}
