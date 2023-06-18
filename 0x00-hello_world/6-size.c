e75b6e7460402f6320e805deac6a9d4e4cd61394
tesfaelias <tesfaon@yahoo.com>
2023-06-18 12:31:48 -0700

    second chance

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
-#!/bin/bash
-#include stdio.h
-/*
- * a program that prints size of a types
- */
+#include<stdio.h>
+/**
+* main is func
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float;
-printf("Size of a Char: %lu byte(s)\n, (unsigned long)sizeof(a));
-return(0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
